5305c102cf3ee04b56af98db8eb97c59dc0baada
OzcarDev <osja1601@gmail.com>
2023-09-06 21:43:21 -0600

    Hands

---
+++ Assets/_VanHelsingVR/Code/Scripts/Animation/HandAnimationController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class HandAnimationController : MonoBehaviour
+{
+
+
+    [SerializeField] private InputActionProperty fistAnimationAction;
+    [SerializeField] private InputActionProperty pointAnimationAction;
+
+    [SerializeField] Animator handAnimator;
+
+    void Update()
+    {
+        float gripvalue = pointAnimationAction.action.ReadValue<float>();
+       float triggervalue = fistAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggervalue);
+        handAnimator.SetFloat("Grip", gripvalue);
+    }
+}
