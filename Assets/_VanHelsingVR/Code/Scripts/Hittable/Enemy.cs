9d1757d68d8b735a025d841105eb167a406f1d93
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-09-17 21:32:34 -0600

    Update Material Property Block

--- Assets/_VanHelsingVR/Code/Scripts/Hittable/Enemy.cs
+++ Assets/_VanHelsingVR/Code/Scripts/Hittable/Enemy.cs
@@ -6,13 +6,33 @@ using UnityEngine;
 public class Enemy : Hittable
 {
     [SerializeField] Renderer[] renderers;
-    private MaterialPropertyBlock _materialRed = new();
-    private MaterialPropertyBlock _materialWhite = new();
+
+    private MaterialPropertyBlock _materialRed;
+    private MaterialPropertyBlock MaterialRed
+    {
+        get
+        {
+            if (_materialRed == null)
+                _materialRed = new();
+            return _materialRed;
+        }
+    }
+    
+    private MaterialPropertyBlock _materialWhite;
+    private MaterialPropertyBlock MaterialWhite
+    {
+        get
+        {
+            if (_materialWhite == null)
+                _materialWhite = new();
+            return _materialWhite;
+        }
+    }
 
     private void Start()
     {
-        _materialRed.SetColor("_Color", Color.red);
-        _materialWhite.SetColor("_Color", Color.white);
+        MaterialRed.SetColor("_Color", Color.red);
+        MaterialWhite.SetColor("_Color", Color.white);
     }
 
     public override void OnHit()
