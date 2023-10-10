fdd1d8778780f86b9f72ae133e10808462192b80
OzcarDev <osja1601@gmail.com>
2023-10-09 23:27:04 -0600

    BloodSystem

---
+++ Assets/_VanHelsingVR/Art/VFX/Code/InvokeDecals.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeDecals : MonoBehaviour
+{
+    public ParticleSystem ps;
+    public GameObject Decal;
+    public DecalManager decalManager;
+    public List<ParticleCollisionEvent> collisionEvents;
+    private void Start()
+    {
+        decalManager = GameObject.Find("DecalManager").GetComponent<DecalManager>();
+        ps = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = ps.GetCollisionEvents(other, collisionEvents);
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            GameObject actualObject = Instantiate(Decal, collisionEvents[i].intersection, Quaternion.identity);
+            StartCoroutine(decalManager.DecalCoroutine(actualObject));
+            i++;
+        }
+       
+    }
+}
