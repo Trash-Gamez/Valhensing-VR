7491d319002497de36bbef693c47e689d4959c89
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-10-18 17:36:41 -0600

    Bye Visua Scripting

---
+++ Assets/RacTools/BehaviourTree/BehaviourTree.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace RacTools.BehaviourTree
+{
+    [CreateAssetMenu(fileName = "BehaviourTree", menuName = "MENUNAME", order = 0)]
+    public class BehaviourTree : ScriptableObject
+    {
+        
+    }
+}
