62f76450abd09c581c22852213a321ad41386f80
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-10-29 11:58:13 -0600

    Transform variable

---
+++ Assets/RacTools/Variables/TransformVariable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace RacTools.Variables
+{
+    [CreateAssetMenu(order = 2, fileName = "TransformVariable", menuName = "GameVariable/Transform")]
+    public class TransformVariable : Variable<Transform>
+    {
+    
+    }
+}
