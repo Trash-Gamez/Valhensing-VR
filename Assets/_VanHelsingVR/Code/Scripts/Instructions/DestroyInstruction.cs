42f4cdc9f9ef778da8e81a49e307243eac96991c
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-10-30 15:47:44 -0600

    Subiendo prueba de puertas

--- Assets/_VanHelsingVR/Code/Scripts/Instructions/DestroyInstruction.cs
+++ Assets/_VanHelsingVR/Code/Scripts/Instructions/DestroyInstruction.cs
@@ -5,8 +5,10 @@ namespace _VanHelsingVR.Instructions
     [DefaultExecutionOrder(1)]
     public class DestroyInstruction : MonoBehaviour
     {
+        public RoomSpawner element;
         public void DestroySelf(float timeToDestroy)
         {
+            element.enemy.Remove(this.gameObject);
             Destroy(gameObject, timeToDestroy);
         }
 
