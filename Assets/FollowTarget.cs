df3d829c0d3a7a974411689254385102c6625683
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-11-11 18:14:32 -0600

    Revert "Revert "Another Try on making enemy look at player""
    This reverts commit 1b6fc1d62ff4d08b1f0563106dd0ca883751462a.

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,12 @@
+using RacTools.Variables;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private VariableReference<Transform> target;
+
+    private void Update()
+    {
+        transform.position = target.Value.position;
+    }
+}
