0b8af813c355628aff276e8732983a7ada66c970
OzcarDev <100314800+OzcarDev@users.noreply.github.com>
2023-11-17 17:04:04 -0600

    Level 1 update

--- Assets/_VanHelsingVR/Code/Scripts/AudioScripts/AudioManager.cs
+++ Assets/_VanHelsingVR/Code/Scripts/AudioScripts/AudioManager.cs
@@ -154,7 +154,7 @@ public class AudioManager : MonoBehaviour
 
 	public void PlaySound3D(string soundName, Vector3 soundPosition)
 	{
-		AudioSource.PlayClipAtPoint(soundLibrary.GetClipFromName(soundName), soundPosition, fxVolume * masterVolume*100);
+		AudioSource.PlayClipAtPoint(soundLibrary.GetClipFromName(soundName), soundPosition, fxVolume * masterVolume*5);
 	}
 
 	public bool IsAmbientPlaying(string clipId)
