b02e56bdba21af78e2db9f9fd66c6cead217e4e6
OzcarDev <100314800+OzcarDev@users.noreply.github.com>
2023-11-17 23:24:56 -0600

    Update Demo Level

--- Assets/_VanHelsingVR/Code/Scripts/Player/Watch.cs
+++ Assets/_VanHelsingVR/Code/Scripts/Player/Watch.cs
@@ -13,20 +13,20 @@ public class Watch : MonoBehaviour
     private Material _bloodMaterial;
     private Material _soulMaterial;
 
-    private float _maxHealth;
+    [SerializeField] VariableReference<int> _maxHealth;
     private float _bloodAmount;
     private float _maxMoney = 999;
     private float _soulAmount;
     private void Start()
     {
-        _maxHealth = health.Value;
+        
         _bloodMaterial = bloodSphere.material;
         _soulMaterial = soulSphere.material;
     }
 
     private void Update()
     {
-        _bloodAmount = (health.Value / _maxHealth) *0.038f ;
+        _bloodAmount = (health.Value / _maxHealth.Value) *0.038f ;
         _bloodMaterial.SetFloat("_LiquidAmount",0.5185f - _bloodAmount);
 
         _soulAmount = (souls.Value / _maxMoney) * 0.018f;
