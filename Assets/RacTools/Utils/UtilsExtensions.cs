fed2782272626a7e0756ffcaef0203a09a1a30c6
Wicho115 <martin.velazquez.luis.alfonso@gmail.com>
2024-02-19 16:08:01 -0600

    Utils & Bootsapper

--- Assets/RacTools/Utils/UtilsExtensions.cs
+++ Assets/RacTools/Utils/UtilsExtensions.cs
@@ -4,39 +4,13 @@ using UnityEngine;
 namespace RacTools.Utils
 {
     public static class UtilsExtensions
-    {
-        /// <summary>
-        /// This Function return a mapped value between two ranges of value
-        /// </summary>
-        /// <param name="in">The In Value that will be evaluate</param>
-        /// <param name="inMin">The Min value used to Map the in param</param>
-        /// <param name="inMax">The Max value used to Map the in param</param>
-        /// <param name="outMin">The Min value used to Map the out value</param>
-        /// <param name="outMax">The Max value used to Map the out value</param>
-        /// <returns>Returns the mapped value from the in param</returns>
-        public static float Map(float inValue, float inMin, float inMax, float outMin, float outMax)
-        {
-            return (inValue - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-        }
-    
-        /// <summary>
-        /// This Function return a mapped value between two ranges of value
-        /// </summary>
-        /// <param name="inValue">The In Value that will be evaluate</param>
-        /// <param name="inRange">The Range of values used to Map de in param</param>
-        /// <param name="outRange">The Range of values used to Map de out value</param>
-        /// <returns>Returns the mapped value from the in param</returns>
-        public static float Map(float inValue, Range inRange, Range outRange)
-        {
-            return Map(inValue, inRange.Min, inRange.Max, outRange.Min, outRange.Max);
-        }
-    
-        private static bool IsNullOrEmpty<T>(this List<T> list)
+    { 
+        public static bool IsNullOrEmpty<T>(this List<T> list)
         {
             return list == null || list.Count <= 0;
         }
 
-        private static T GetOrAdd<T>(this GameObject gameObject, bool warningIfAddingComponent = false) where T : MonoBehaviour
+        public static T GetOrAdd<T>(this GameObject gameObject, bool warningIfAddingComponent = false) where T : MonoBehaviour
         {
             if (gameObject.TryGetComponent<T>(out var component)) return component;
         
@@ -46,5 +20,9 @@ namespace RacTools.Utils
             component = gameObject.AddComponent<T>();
             return component;
         }
+
+        public static void Log(this string message) => Debug.Log(message);
+        public static void LogWarning(this string message) => Debug.LogWarning(message);
+        public static void LogError(this string message) => Debug.LogError(message);
     }
 }
--- Assets/_VanHelsingVR/Code/Scripts/Initialization/Bootstrapper.cs
+++ Assets/_VanHelsingVR/Code/Scripts/Initialization/Bootstrapper.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using RacTools.Variables;
+using System.Linq;
+using System.Reflection;
+
 using UnityEngine;
+using RacTools.Utils;
 
 public class Bootstrapper
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Execute()
     {
-        Debug.Log("Execute Bootstrapper");
+        
     }
 }
