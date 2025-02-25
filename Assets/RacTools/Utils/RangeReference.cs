8028250c792d6fddd7b76707c99347c2428ed695
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-02-24 20:27:51 -0600

    Ractool Utils Added

---
+++ Assets/RacTools/Utils/RangeReference.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RacTools.Utilities
+{
+    [Serializable]
+    public class Rangereference
+    {
+        //TODO: Make a class that can be used to referenc variables in Ranges
+        //[SerializeField] private 
+    }
+}
