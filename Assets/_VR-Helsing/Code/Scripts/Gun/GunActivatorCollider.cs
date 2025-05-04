86a7b9b4bc4b1db8212a2dc39cd5477d60848059
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-05-04 12:36:57 -0600

    Weapon Activation on head
    Se añadio la mecanica de cambio de arma en cabeza

--- Assets/_VR-Helsing/Code/Scripts/Gun/GunActivatorCollider.cs
+++ Assets/_VR-Helsing/Code/Scripts/Gun/GunActivatorCollider.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using Autohand;
 using UnityEngine;
-using UnityEngine.Serialization;
+
 
 namespace _VR_Helsing.Gun
 {
@@ -8,9 +9,19 @@ namespace _VR_Helsing.Gun
     {
         [SerializeField] private NEW_GunHolder gunHolder;
         [SerializeField] private string handTag;
-        
+
+        private bool _canDetect = false;
+
+        private IEnumerator Start()
+        {
+            _canDetect = false;
+            yield return new WaitForSeconds(0.1f); //Wait Until player ready:
+            _canDetect = true;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
+            if (!_canDetect) return;
             if (!other.CompareTag(handTag)) return;
             if (!other.TryGetComponent(out Hand hand)) return;
             
