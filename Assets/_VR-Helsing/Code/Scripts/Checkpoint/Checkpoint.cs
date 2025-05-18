b75ab8efba3beb4fc06527bc93e6ad7c64f99d12
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-05-17 23:28:32 -0600

    Checkpoints logic
    El Jugador ahora tiene el manejador para los checkpoints y la logica esta por implementarse

--- Assets/_VR-Helsing/Code/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/_VR-Helsing/Code/Scripts/Checkpoint/Checkpoint.cs
@@ -3,17 +3,5 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class Checkpoint : MonoBehaviour
 {
-    private BoxCollider _boxCollider;
-
-    private void Start()
-    {
-        if(!_boxCollider) _boxCollider = GetComponent<BoxCollider>();
-
-        _boxCollider.isTrigger = true;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
-    }
+    
 }
