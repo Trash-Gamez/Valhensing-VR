60e5bfd364571d202769b4387934240c5497e1c2
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-05-19 17:41:14 -0600

    Checkpoint in Scene

--- Assets/_VR-Helsing/Code/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/_VR-Helsing/Code/Scripts/Checkpoint/Checkpoint.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField] private Transform returnPoint;
+
+    public Transform ReturnPoint => returnPoint;
+    
     
 }
