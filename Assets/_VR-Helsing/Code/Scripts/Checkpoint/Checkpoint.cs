a828be484ae349ed6ab06c9887f1f493be6e2bcc
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-05-19 20:10:31 -0600

    MoreCheckpoints

--- Assets/_VR-Helsing/Code/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/_VR-Helsing/Code/Scripts/Checkpoint/Checkpoint.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Collider))]
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] private Transform returnPoint;
