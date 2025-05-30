d27c319793097c51fcef3ceca87196c07c94bffa
Ozcar <100314800+OzcarDev@users.noreply.github.com>
2025-05-30 00:36:02 -0600

    Take Foto System

---
+++ Assets/_VR-Helsing/Code/Scripts/_OLD/ChangeName.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ChangeName : MonoBehaviour
+{
+   public void ChangeObjectName()
+    {
+        gameObject.name = "Empty";
+    }
+}
---
+++ Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TakeFoto : MonoBehaviour
+{
+
+    [SerializeField] private int multiplier;
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("ScreenShot");
+			ScreenCapture.CaptureScreenshot("screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", multiplier);
+		}
+	}
+}
