41aa9e2a378e51961264b6dda9c2e8e2ffb5d881
Santymr <100314893+SantiDEV2@users.noreply.github.com>
2025-06-04 12:58:09 -0600

    asd

--- Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
+++ Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
@@ -5,14 +5,27 @@ using UnityEngine.InputSystem;
 
 public class TakeFoto : MonoBehaviour
 {
-
     [SerializeField] private int multiplier;
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Debug.Log("ScreenShot");
-			ScreenCapture.CaptureScreenshot("screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", multiplier);
-		}
-	}
+
+    private InputAction screenshotAction;
+
+    private void OnEnable()
+    {
+        screenshotAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/space");
+        screenshotAction.performed += OnScreenshot;
+        screenshotAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        screenshotAction.performed -= OnScreenshot;
+        screenshotAction.Disable();
+        screenshotAction.Dispose();
+    }
+
+    private void OnScreenshot(InputAction.CallbackContext context)
+    {
+        Debug.Log("ScreenShot");
+        ScreenCapture.CaptureScreenshot("screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", multiplier);
+    }
 }
