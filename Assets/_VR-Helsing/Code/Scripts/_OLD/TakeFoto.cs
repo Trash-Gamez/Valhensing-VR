b363149dfcf48bb7d4b87c5c6250095fa984f026
Ozcar <100314800+OzcarDev@users.noreply.github.com>
2025-09-03 10:59:30 -0600

    LastChanges

--- Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
+++ Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
@@ -7,12 +7,11 @@ public class TakeFoto : MonoBehaviour
 {
 
     [SerializeField] private int multiplier;
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Debug.Log("ScreenShot");
-			ScreenCapture.CaptureScreenshot("screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", multiplier);
-		}
-	}
+	
+
+    private void Start()
+    {
+        Debug.Log("ScreenShot");
+        ScreenCapture.CaptureScreenshot("screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", multiplier);
+    }
 }
