85c93bfa82174d7111eb05eccabc7b546b92fc4e
EdsonGP <95706015+EdsonGP723@users.noreply.github.com>
2025-09-03 12:50:34 -0600

    Tomando screenshots

--- Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
+++ Assets/_VR-Helsing/Code/Scripts/_OLD/TakeFoto.cs
@@ -7,11 +7,59 @@ public class TakeFoto : MonoBehaviour
 {
 
     [SerializeField] private int multiplier;
-	
+    public InputActionReference takeFotoAction;
+    public Camera cam;
 
-    private void Start()
+    private void OnEnable()
+    {
+        takeFotoAction.action.Enable();
+        takeFotoAction.action.performed += OnTakePhotoPerformed;
+    }
+
+    private void OnDisable()
+    {
+        takeFotoAction.action.performed -= OnTakePhotoPerformed;
+        takeFotoAction.action.Disable();
+    }
+
+    private void OnTakePhotoPerformed(InputAction.CallbackContext context)
+    {
+        Take();
+    }
+
+    public void Take()
     {
         Debug.Log("ScreenShot");
-        ScreenCapture.CaptureScreenshot("screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", multiplier);
+        string filename = "screenshot " + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png";
+#if UNITY_EDITOR
+        StartCoroutine(CaptureScreenshotHighRes(filename, multiplier));
+#else
+        ScreenCapture.CaptureScreenshot(filename, multiplier);
+#endif
+    }
+
+    private IEnumerator CaptureScreenshotHighRes(string filename, int multiplier)
+    {
+        yield return new WaitForEndOfFrame();
+        if (cam == null)
+        {
+            Debug.LogError("No main camera found for screenshot.");
+            yield break;
+        }
+        int width = Screen.width * multiplier;
+        int height = Screen.height * multiplier;
+        RenderTexture rt = new RenderTexture(width, height, 24);
+        cam.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(width, height, TextureFormat.RGB24, false);
+        cam.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        screenShot.Apply();
+        cam.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(rt);
+        byte[] bytes = screenShot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(filename, bytes);
+        Debug.Log($"Screenshot saved to {filename}");
     }
 }
