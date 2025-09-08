b24d46037dc437c43af956e43a7c2e9c6ddc55c4
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-09-08 10:00:09 -0600

    Added Gun Handler Script
    New Main Handler Gun Script

---
+++ Assets/_VR-Helsing/Code/Scripts/Gun/GunHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace _VR_Helsing.Gun
+{
+    /// <summary>
+    /// The main class that handles all Gun Logic
+    /// </summary>
+    public class GunHandler : MonoBehaviour
+    {
+    
+        void Update()
+        {
+            
+        }
+    }
+}
