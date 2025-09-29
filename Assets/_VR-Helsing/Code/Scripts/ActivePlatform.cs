263008a998dd8ffa33dbca20e69a77839975f20d
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-09-29 14:24:27 -0600

    Update ActivePlatform.cs
    Añadida flag que permite la detección 1 sola vez de la plataforma

--- Assets/_VR-Helsing/Code/Scripts/ActivePlatform.cs
+++ Assets/_VR-Helsing/Code/Scripts/ActivePlatform.cs
@@ -7,10 +7,14 @@ public class ActivePlatform : MonoBehaviour
     [SerializeField] private GameObject cageWalls;
     [SerializeField] private PlatformController platformController;
 
+    private bool _triggered;
+
     private void OnTriggerEnter(Collider other)
     {
+        if (_triggered) return;
         if (other.CompareTag("Player"))
         {
+            _triggered = true;
             cageWalls.SetActive(true);
             platformController.StartNextPoint(3);
             gameObject.SetActive(false);
